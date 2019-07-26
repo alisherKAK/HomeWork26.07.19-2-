03aa25ad4c71b8eea155067f9b33732c38b21cdb
alisherKak <alisher.alish333@gmail.com>
2019-07-27 02:03:44 +0600

    Добавьте файлы проекта.

---
+++ HomeWork26_07_19_2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeWork26_07_19_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
